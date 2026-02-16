c76952e54588c3b63a316f52e506c9045e1a08f4
Oleksandr Hordiienko <g.aleks2308@gmail.com>
2026-02-16 16:13:42 +0200

    add task solutions

--- src/components/TabsPage/TabsPage.tsx
+++ src/components/TabsPage/TabsPage.tsx
@@ -1,34 +1,46 @@
 import React, { useContext } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { TabsContext } from '../../context';
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 export const TabsPage: React.FC = () => {
+  const navigate = useNavigate();
   const { tabId } = useParams<{ tabId: string }>();
   const tabs = useContext(TabsContext) ?? [];
-  const activeTab = tabs.find(tab => tab.id === tabId);
+
+  const selectedIndex = tabs.findIndex(tab => tab.id === tabId);
+
+  const handleSelect = (index: number) => {
+    const tab = tabs[index];
+
+    if (tab) {
+      navigate(`/tabs/${tab.id}`);
+    }
+  };
 
   return (
     <>
       <h1 className="title">Tabs page</h1>
-      <div className="tabs is-boxed">
-        <ul>
-          {tabs.map(tab => (
-            <li
-              key={tab.id}
-              data-cy="Tab"
-              className={tab.id === tabId ? 'is-active' : ''}
-            >
-              <Link to={`/tabs/${tab.id}`} data-cy="TabLink">
-                {tab.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
 
-      <div className="block" data-cy="TabContent">
-        {activeTab ? activeTab.content : 'Please select a tab'}
-      </div>
+      <Tabs
+        selectedIndex={selectedIndex === -1 ? 0 : selectedIndex}
+        onSelect={handleSelect}
+      >
+        <TabList>
+          {tabs.map(tab => (
+            <Tab key={tab.id} data-cy="Tab">
+              {tab.title}
+            </Tab>
+          ))}
+        </TabList>
+
+        {tabs.map(tab => (
+          <TabPanel key={tab.id} data-cy="TabContent">
+            {tab.content}
+          </TabPanel>
+        ))}
+      </Tabs>
     </>
   );
 };
